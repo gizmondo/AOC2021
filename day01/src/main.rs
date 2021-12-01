e342b5f58167578b50ad085cebe41de8a7764b1f
Alex Aktsipetrov <alex.akts@gmail.com>
2021-12-01 18:00:17 +0100

    cargo fmt

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -5,7 +5,10 @@ use std::io::{self, Read};
 fn main() -> Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    let depths = input.split_whitespace().map(str::parse::<i32>).collect::<Result<Vec<i32>, _>>()?;
+    let depths = input
+        .split_whitespace()
+        .map(str::parse::<i32>)
+        .collect::<Result<Vec<i32>, _>>()?;
 
     part1(&depths);
     part2(&depths);
@@ -26,7 +29,9 @@ fn part1(depths: &Vec<i32>) {
     println!("{}", count_increasing(depths));
 }
 
-fn part2(depths: &Vec<i32>)  {
-    let windows: Vec<_> = izip!(depths, &depths[1..], &depths[2..]).map(|(a, b, c)| a + b + c).collect();
+fn part2(depths: &Vec<i32>) {
+    let windows: Vec<_> = izip!(depths, &depths[1..], &depths[2..])
+        .map(|(a, b, c)| a + b + c)
+        .collect();
     println!("{}", count_increasing(&windows));
 }
